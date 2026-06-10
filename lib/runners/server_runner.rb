6e9c3f2a4b2e0de8618e3dbf4d6a6132bd5b3f43
SilasNS <silasschlax@gmail.com>
2026-06-10 10:41:54 -0400

    copy runners

---
+++ lib/runners/server_runner.rb
@@ -0,0 +1,15 @@
+require_relative '../server/socket_server'
+
+server = SocketServer.new
+server.start
+while true do
+  begin
+    server.accept_new_client
+    game = server.create_game_if_possible
+    if game
+      server.run_game(game)
+    end
+  rescue
+    server.stop
+  end
+end
