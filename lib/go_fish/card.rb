338a1eff5f2b79276fc7daafb01b7017d1059584
SilasNS <silasschlax@gmail.com>
2026-06-10 19:07:59 -0400

    Rebuild card class

---
+++ lib/go_fish/card.rb
@@ -0,0 +1,30 @@
+# Card class
+class Card
+  attr_reader :rank, :suit
+
+  class InvalidRank < StandardError; end
+  class InvalidSuit < StandardError; end
+
+  RANKS = %w[2 3 4 5 6 7 8 9 10 J Q K A].freeze
+  SUITS = %w[Spades Diamonds Hearts Clubs].freeze
+
+  def initialize(rank, suit = 'Spades')
+    raise InvalidRank unless RANKS.include?(rank)
+    raise InvalidSuit unless SUITS.include?(suit)
+
+    @rank = rank
+    @suit = suit
+  end
+
+  def to_s
+    "#{rank} of #{suit}"
+  end
+
+  def ==(other)
+    rank == other.rank && suit == other.suit
+  end
+
+  def self.valid_rank?(rank)
+    RANKS.include?(rank.upcase)
+  end
+end
---
+++ spec/go_fish/card_spec.rb
@@ -0,0 +1,47 @@
+require_relative '../../lib/go_fish/card'
+
+describe Card do
+  it 'has a rank, suit, and value' do
+    card = described_class.new('A', 'Spades')
+    expect(card.rank).to eq 'A'
+    expect(card.suit).to eq 'Spades'
+  end
+
+  it 'cards of the same rank and suit are equal' do
+    card1 = described_class.new('A', 'Spades')
+    card2 = described_class.new('K', 'Spades')
+    card3 = described_class.new('A', 'Spades')
+
+    expect(card1).not_to eq card2
+    expect(card1).to eq card3
+  end
+
+  it 'should allow valid ranks' do
+    expect {
+      described_class.new('15', 'Spades')
+    }.to raise_error Card::InvalidRank
+  end
+
+  it 'should allow valid suits' do
+    expect {
+      described_class.new('3', 'Bulkogi')
+    }.to raise_error Card::InvalidSuit
+  end
+  describe '#to_s' do
+    let(:card) { described_class.new('A')}
+    it 'returns card as formatted string' do
+      expected_output = 'A of Spades'
+      expect(card.to_s).to eq expected_output
+    end
+  end
+  describe '.valid_rank?' do
+    it 'returns false if invalid rank' do
+      rank = 'L'
+      expect(described_class.valid_rank?(rank)).to be false
+    end
+    it 'returns true if valid rank' do
+      rank = 'K'
+      expect(described_class.valid_rank?(rank)).to be true
+    end
+  end
+end
