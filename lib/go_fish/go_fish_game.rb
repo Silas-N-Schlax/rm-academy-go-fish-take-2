9a2f7819855b562b73d1f5fc21714a52519a0936
SilasNS <silasschlax@gmail.com>
2026-06-12 08:00:02 -0400

    Implement Game Logic (WIP).
    Refactors to meet constraints coming

--- lib/go_fish/go_fish_game.rb
+++ lib/go_fish/go_fish_game.rb
@@ -1,8 +1,9 @@
 require_relative 'deck'
 require_relative 'card'
+require_relative 'turn_result'
 # Go Fish Game Class
 class GoFishGame
-  attr_accessor :deck, :current_user_idx
+  attr_accessor :deck, :current_user_idx, :results
   attr_reader :users
 
   SMALL_HAND = 5
@@ -17,9 +18,36 @@ class GoFishGame
   end
 
   def start
+    # deck.cards = [Card.new('J')]
+    # users.first.player.hand = [Card.new('J'), Card.new('J')]
+    # users.last.player.hand = [Card.new('J')]
+    # ^ For Testing Server and Sockets Only
     deck.shuffle_deck
     deal
   end
+
+  def run_turn(user_id, rank)
+    return if winner
+    return unless find_user(user_id)
+
+    # ^ Need since its checked in GameSession?
+    user_in_question = find_user(user_id)
+    cards = user_in_question.player.take_cards_of_rank(rank)
+
+    current_user.player.add_cards(cards) unless cards.empty?
+    fishing_card = go_fish(rank) if cards.empty?
+    generate_turn_result(user_in_question, rank, cards, fishing_card, cards.empty? && fishing_card.nil? ? false : true)
+  end
+  # ^ Refactor
+
+  def winner
+    winning_user if deck.empty? && users.all? { |user| user.player.empty_hand? }
+  end
+
+  def find_user(id)
+    users.select { |user| user.id == id }.first
+  end
+  # ^ Private?
 
   def current_user
     users[current_user_idx]
@@ -47,6 +75,53 @@ class GoFishGame
         user.player.add_cards([deck.top_card])
       end
     end
+  end
+
+  def go_fish(rank)
+    card = deck.top_card
+    return next_user_turn if card.nil?
+
+    current_user.player.add_cards([card])
+    next_user_turn unless card.rank == rank
+    card
+  end
+  # ^ Refactor
+
+  def next_user_turn
+    new_index = current_user_idx + 1
+    first_user_idx = 0
+    self.current_user_idx = new_index > users.size - 1 ? first_user_idx : new_index
+  end
+  # ^ Refactor
+
+  def generate_turn_result(opponent, rank, cards, card_picked_up, goes_again)
+    self.results = TurnResult.new(
+      current_user: current_user, opponent: opponent,
+      card_asked_for: rank, cards_taken: cards,
+      card_picked_up: card_picked_up, goes_again: goes_again
+    )
+  end
+  # ^ Refactor
+
+  def winning_user
+    winning_users = []
+    users.each do |user|
+      winning_users << user if winning_users.empty? || winning_users.first.player.books_size == user.player.books_size
+      winning_users = [user] if user.player.books_size > winning_users.first.player.books_size
+    end
+    return player_highest_book_value(winning_users) if winning_users.size > 1
+
+    winning_users.first
+  end
+
+  def player_highest_book_value(tied_users)
+    current_winner = [nil, nil]
+    tied_users.each do |user|
+      user.player.books.each do |book|
+        current_winner = [user, book] if current_winner[1].nil? || book.value > current_winner[1].value
+      end
+    end
+    current_winner.first
   end
 
   def number_of_cards_to_deal
--- spec/go_fish/go_fish_game_spec.rb
+++ spec/go_fish/go_fish_game_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../../lib/go_fish/card'
 require_relative '../../lib/server/user'
 require_relative '../../lib/go_fish/go_fish_game'
+require_relative '../../lib/go_fish/book'
 
 describe GoFishGame do
   let(:user1) { User.new('client', 1) }
@@ -46,40 +47,195 @@ describe GoFishGame do
     end
   end
 
+  describe '#find_player' do
+    let(:game) { described_class.new([user1, user2]) }
+    context 'when provided with id for user1' do
+      it 'returns user1' do
+        user1_id = 1
+        result = game.find_user(user1_id)
+        expect(result.name).to eq user1.name
+      end
+    end
+
+    context 'when provided with an id for a non-existent player' do
+      it 'returns nil' do
+        user3_id = 3
+        result = game.find_user(user3_id)
+        expect(result).to be_nil
+      end
+    end
+  end
+
+  describe '#run_turn' do
+    let(:card1) { Card.new('A') }
+    context 'when a turn is run with 2 players' do
+      context 'when user1 is asking user2 for a card they have' do
+        let(:game) { described_class.new([user1, user2]) }
+        let!(:user1_data) { game.users.first }
+        let!(:user2_data) { game.users.last }
+        before do
+          user2_data.player.hand << card1
+          game.run_turn(2, 'A')
+        end
+        it 'player 1 gets the cards added to their hand' do
+          expected_hand_size = 1
+          expect(user1_data.player.hand_size).to eq expected_hand_size
+        end
+        it 'user2 gets the cards removed from their hand' do
+          expect(user2_data.player.hand_size).to be_zero
+        end
+        context 'when user1 asks user2 for a card user2 does not have' do
+          before do
+            game.run_turn(2, 'J')
+            game.deck.cards.unshift Card.new('10')
+            # ^ Work?
+          end
+          context 'user1 does not take a card from user2' do
+            it 'card is added to user1 hand from deck' do
+              expected_hand_size = 2
+              expect(user1_data.player.hand_size).to eq expected_hand_size
+            end
+            it 'current player is set to user2' do
+              expect(game.current_user.name).to be user2_data.name
+            end
+          end
+        end
+        it 'returns a valid round result' do
+          expect(game.results).to be_a TurnResult
+        end
+      end
+      context 'when user1 asks for a card user2 does have and go fishing' do
+        let(:game) { described_class.new([user1, user2]) }
+        let!(:user1_data) { game.users.first }
+        context 'when they pick up that card' do
+          before do
+            game.deck.cards.unshift(Card.new('A'))
+            game.run_turn(2, 'A')
+          end
+          it 'adds card to their hand' do
+            expected_hand_size = 1
+            expect(user1_data.player.hand_size).to eq expected_hand_size
+          end
+          it 'they are still current player' do
+            expect(game.current_user.name).to eq user1_data.name
+          end
+        end
+      end
+      context 'when user1 asks a player that does not exist' do
+        let(:game) { described_class.new([user1, user2]) }
+        it 'returns nil' do
+          expect(game.run_turn(3, 'J')).to be nil
+        end
+      end
+      context 'when user1 is asking user2 for a card they do not have' do
+        let(:game) { described_class.new([user1, user2]) }
+        let!(:user1_data) { game.users.first }
+        let!(:user2_data) { game.users.last }
+        context 'when user1 does not pick up that card' do
+          before do
+            game.current_user_idx = 1
+            game.run_turn(1, 'A')
+          end
+          it 'card is added to user1 hand' do
+            expected_hand_size = 1
+            expect(user2_data.player.hand_size).to eq expected_hand_size
+          end
+          it 'current player is set to next player in queue' do
+            expect(game.current_user.name).to eq user1_data.name
+          end
+        end
+      end
+      context 'when there deck is empty and a player goes fishing' do
+        let(:game) { described_class.new([user1, user2]) }
+        let!(:user2_data) { game.users.last }
+        before do
+          game.deck.cards = []
+          game.run_turn(1, 'A')
+        end
+        it 'does not give the player a card' do
+          expect(user2_data.player.hand_size).to be_zero
+        end
+        it 'sets the current player to next player in the queue' do
+          expect(game.current_user.name).to eq user2_data.name
+        end
+      end
+    end
+  end
+  describe '#winner?' do
+    let!(:game) { described_class.new([user1, user2]) }
+    context 'when there is no winner' do
+      it 'returns' do
+        expect(game.winner).to be_nil
+      end
+      context 'when the deck is empty and all user hands are empty' do
+        let!(:game_user1) { game.users.first }
+        let!(:game_user2) { game.users.last }
+        before do
+          game.deck = []
+          game_user1.player.hand = []
+          game_user1.player.books = [Book.new('K'), Book.new('2')]
+          game_user2.player.hand = []
+          game_user2.player.books = [Book.new('J')]
+        end
+        it 'returns the player with the most books' do
+          expect(game.winner.id).to be game_user1.id
+        end
+        context 'when there is a tie for most books' do
+          it 'returns the player with the highest book' do
+            game_user1.player.books.pop
+            expect(game.winner).to be game_user1
+          end
+        end
+      end
+    end
+  end
+
  # ^ Validation methods
   describe '#valid_rank?' do
     let(:game) { described_class.new([user1, user2]) }
-    it 'returns false if rank is invalid' do
-      invalid_rank = 'L'
-      expect(game.valid_rank?(invalid_rank)).to be false
+    context 'when the rank provided is a not valid standard rank' do
+      it 'returns false' do
+        invalid_rank = 'L'
+        expect(game.valid_rank?(invalid_rank)).to be false
+      end
     end
-    it 'returns true if rank is valid' do
-      valid_rank = 'K'
-      expect(game.valid_rank?(valid_rank)).to be true
+    context 'when the rank provided is a valid standard rank' do
+      it 'returns true' do
+        valid_rank = 'K'
+        expect(game.valid_rank?(valid_rank)).to be true
+      end
     end
   end
   describe '#card?' do
     let(:game) { described_class.new([user1, user2]) }
     let(:rank_asked_for) { 'K' }
-    it 'returns false if player does not have that rank' do
-      expect(game.card?(rank_asked_for)).to be false
+    context 'when the player does not have the card they asked for' do
+      it 'returns false' do
+        expect(game.card?(rank_asked_for)).to be false
+      end
     end
-    it 'returns true if player does have rank' do
-      game.current_user.player.add_cards([Card.new('K')])
-      expect(game.card?(rank_asked_for)).to be true
+    context 'when the player does have the card they asked for' do
+      it 'returns true' do
+        game.current_user.player.add_cards([Card.new('K')])
+        expect(game.card?(rank_asked_for)).to be true
+      end
     end
   end
   describe '#turn_skipped?' do
     let(:game) { described_class.new([user1, user2]) }
     let(:user) { game.current_user }
-    it 'returns false when player can play' do
-      game.start
-      expect(game.turn_skipped?).to be false
+    context 'when the players hand and/or the deck is not empty' do
+      it 'returns false' do
+        game.start
+        expect(game.turn_skipped?).to be false
+      end
     end
-    it 'returns true when player turn is skipped' do
-      game.deck = []
-      user.player.hand = []
-      expect(game.turn_skipped?).to be true
+    context 'when the players hand and deck is empty' do
+      it 'returns true' do
+        game.deck = []
+        user.player.hand = []
+        expect(game.turn_skipped?).to be true
+      end
     end
   end
 end
